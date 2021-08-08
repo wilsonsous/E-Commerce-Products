b0199edefd36e9980338d5a3d5f5bd438948646d
Franklyn Diaz <franklyn_diaz@hotmail.com>
2021-08-08 10:57:35 -0400

    adding database.js

---
+++ controllers/database.js
@@ -0,0 +1,51 @@
+const mysql = require('mysql2');
+
+let connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+   // passwod: '',
+    database: 'e_commerce_prj',
+});
+
+module.exports = connection;
+
+function connect() {
+    console.log(connection.connect());
+    connection.
+    console.log('connect');
+    console.log(connection.query('USE e_commerce_prj')); //select the database 
+}
+
+connect();
+
+function getClientNames(){
+    console.log (connection.query('DESCRIBE clients'));
+}
+function getClients(){
+  
+    connection.query("SELECT * FROM clients", function (err, result, fields) {
+      if (err) throw err;
+      console.log(result);
+    });
+}
+
+function login() {
+    
+}
+
+function getprudoct() {
+    
+}
+
+function addProduct() {
+    
+}
+
+
+function disconnect() {
+    connection.end();
+    console.log('disconnecting...');
+}
+
+
+disconnect();
