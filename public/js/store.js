b4a1b45e881de978ca6456a24d628a12847f821c
logon pierce <logonpierce@gmail.com>
2021-08-09 17:06:24 -0400

    updating handlebars

--- public/js/store.js
+++ public/js/store.js
@@ -23,7 +23,7 @@ function ready() {
         button.addEventListener('click', addToCartClicked)
     }
 
-    document.getElementsByClassName('btn-purchase')[0].addEventListener('click', purchaseClicked)
+    //document.getElementsByClassName('btn-purchase')[0].addEventListener('click', purchaseClicked)
 }
 
 function purchaseClicked() {
