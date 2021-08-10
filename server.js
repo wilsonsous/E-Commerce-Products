6ef1ee9ee1735f026ee417ba8ea4266a4343dcda
Mason Hames <m.hames2@yahoo.com>
2021-08-09 20:11:58 -0400

    pulled data from query for products and displayed on the store page

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const expbs = require('express-handlebars')
 const path = require('path');
 const sequelize = require('./config/connection');
+const { Product } = require('./models');
 const PORT = process.env.PORT || 8080;
 // Handlebars settings
 app.engine('handlebars', expbs());
@@ -14,8 +15,10 @@ app.get('/', (req, res) => {
     res.render('index');
 });
 
-app.get('/store', (req, res) => {
-    res.render('store');
+app.get('/store', async (req, res) => {
+    const productData = await Product.findAll({});
+    const products = productData.map(product => product.get({ plain: true }));
+    res.render('store', { products });
 });
 
 app.get('/login', (req, res) => {
